925a4ace05d00a987da103affc82bf6a645d58a9
abariba <0171359@student.roc-nijmegen.nl>
2018-07-18 13:37:33 +0200

    well im stuck

--- HealthModuleMod/Class1.cs
+++ HealthModuleMod/Class1.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System;
+//using HModFabricator;
+using SMLHelper;
+using SMLHelper.Patchers;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using UnityEngine;
+using Harmony;
 
 namespace HealthModuleMod
 {
-    public class Class1
+
+    //using System.Collections.Generic;
+    //using SMLHelper.V2.Crafting;
+    //using SMLHelper.V2.Assets;
+    //using UnityEngine;
+    class AddHealthModule
     {
-        e
+        public static TechType FlatHealthModule = TechTypeHandler.AddTechType("FHM", "Subdurmal Body Armor", "this is a subdurmal body armor upgrade.", SpriteManager.Get(TechType.FirstAidKit));
+
+
+        public static void patch()
+        {
+            CraftDataHandler.SetEquipmentType(FlatHealthModule, EquipmentType.Chip);
+            Console.WriteLine("[healthchip] Initialized");
+            var harmony = HarmonyInstance.Create("abariba.HealthModuleMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Console.WriteLine("[healthchip]harmony Initialized");
+        }
     }
 }
---
+++ HealthModuleMod/MaxhealthModefier.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+
+namespace HealthModuleMod
+{
+    [HarmonyPatch(typeof(Survival))]//Surivial
+    [HarmonyPatch("Reset")]
+    class Survival_Reset_Patch
+    {
+        public static bool Prefix(Survival __instance)
+        {
+            //Equipment equipment = __instance.;
+            float extrahealth = 0f;
+            bool healthchipEquipped = true;//__instance.GetComponent("healthchip");
+            if (healthchipEquipped)
+            {
+                extrahealth = 50f;
+            }
+            __instance.GetComponent<Player>().liveMixin.data.maxHealth = 100f + extrahealth;
+            __instance.GetComponent<Player>().liveMixin.health = 100F * 0f;
+
+            //__instance.food = HCPSettings.Instance.FoodStart;
+            //__instance.water = HCPSettings.Instance.WaterStart;
+
+            return false;
+        }
+    }
+}
